4da8f29627d5200c6d95a99070cff7e7c146fd52
craigaloewen <craigaloewen@gmail.com>
2017-04-17 20:43:45 -0400

    Added

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-<<<<<<< HEAD
+
 module LongDistanceLetters
-=======
-module TestDbApp
->>>>>>> 2a6ff49e845a908f17287f4cef55ce97623ea04b
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
