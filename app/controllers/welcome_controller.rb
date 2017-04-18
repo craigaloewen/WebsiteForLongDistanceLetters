b689414d37a6943401eccd7ff4c58efd6bedc6b0
craigaloewen <craigaloewen@gmail.com>
2017-04-18 15:35:07 -0400

    Fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-  	if !session[:viewNumber]
-  		session[:viewNumber] = 0
-
-  	session[:viewNumber] = session[:viewNumber] + 1
+  	
 
   end
 
