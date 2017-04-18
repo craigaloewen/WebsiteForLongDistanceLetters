15cc910d869aade9579623f7bf17838969300168
craigaloewen <craigaloewen@gmail.com>
2017-04-17 21:37:05 -0400

    Fix

--- app/controllers/love_letters_controller.rb
+++ app/controllers/love_letters_controller.rb
@@ -1,8 +1,27 @@
 class LoveLettersController < ApplicationController
+	
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+
 	def new
 	end
 
+	
 	def create
-		render plain: params[:love_letter].inspect
+		@love_letter = LoveLetter.new(love_letter_params)
+
+		@love_letter.save
+		redirect_to @love_letter
+	end
+
+	private
+	def love_letter_params
+		params.require(:love_letter).permit(:title, :text)
 	end
 end
