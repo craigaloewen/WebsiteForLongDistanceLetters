4f086737e9e81aa4c4bfd9a33a885996d4152c7b
craigaloewen <craigaloewen@gmail.com>
2017-04-18 15:47:23 -0400

    Fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,10 @@
 class WelcomeController < ApplicationController
   def index
   	
-  	if !(session[:value])
-  		session[:value] = 1
-  	else 
-  		session[:value] = 5
+  	if !(session[:value]) then session[:value] = 1
+
+  	session[:value] = session[:value] + 1
+
 
   	render :text => session[:value]
 
