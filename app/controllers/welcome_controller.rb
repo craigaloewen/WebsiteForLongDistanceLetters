200584b979fd725ec038ef78b16225abeea9f37a
craigaloewen <craigaloewen@gmail.com>
2017-04-18 15:29:55 -0400

    Fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+  	if !session[:viewNumber]
+  		session[:viewNumber] = 0
+
+  	session[:viewNumber] = session[:viewNumber] + 1
+
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
   resources :love_letters
+
+  
  
   root 'welcome#index'
 end
